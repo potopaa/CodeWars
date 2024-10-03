e051b6e82e6e3fc3b13d260b1e41da92ba7278db
potopaa <potopaa@gmail.com>
2024-10-03 21:57:07 +0300

    Create Convert to Binary.py

---
+++ Convert to Binary.py
@@ -0,0 +1,17 @@
+"""
+
+Given a non-negative integer b, write a function which returns an integer d such that the binary
+representation of b is the same as the decimal representation of d.
+
+Examples:
+
+1  ->    1  (because  1 is    0b1 in binary)
+5  ->  101  (because  5 is  0b101 in binary)
+11 -> 1011  (because 11 is 0b1011 in binary)
+
+
+"""
+
+
+def to_binary(n):
+    return int(f'{n:b}')
