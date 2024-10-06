3c66a11ff4ed00ad45a1166d69d1af43d8573961
potopaa <potopaa@gmail.com>
2024-10-06 22:38:59 +0300

    Create Third Angle of a Triangle.py

---
+++ Third Angle of a Triangle.py
@@ -0,0 +1,17 @@
+"""
+
+You are given two interior angles (in degrees) of a triangle.
+
+Write a function to return the 3rd.
+
+Note: only positive integers will be tested.
+
+"""
+
+
+def other_angle(a, b):
+    if (a or b) < 0:
+        return "angle cannot be smaller than 0"
+
+    else:
+        return 180 - (a + b)
