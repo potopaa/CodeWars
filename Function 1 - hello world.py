8c947e072ac528822ab37d8f3a26fea2d62fd382
potopaa <potopaa@gmail.com>
2024-10-07 23:29:01 +0300

    Create Function 1 - hello world.py

---
+++ Function 1 - hello world.py
@@ -0,0 +1,9 @@
+"""
+
+Make a simple function called greet that returns the most-famous "hello world!".
+
+"""
+
+# Write a function `greet` that returns "hello world!"
+def greet():
+    return "hello world!"
