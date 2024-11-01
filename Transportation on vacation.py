e97810ea4e558ebbaafeb8dcffb80a1a823722a3
potopaa <156847443+potopaa@users.noreply.github.com>
2024-11-01 22:21:22 +0200

    Update Transportation on vacation.py

--- Transportation on vacation.py
+++ Transportation on vacation.py
@@ -1,5 +1,4 @@
 """
-
 After a hard quarter in the office you decide to get some rest on a vacation.
 So you will book a flight for you and your girlfriend and try to leave all the mess behind you.
 
